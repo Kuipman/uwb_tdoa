874d46c054871ae05470c57c3f69cc6ac32c2dfc
Nick <nkuipers@ucsc.edu>
2023-11-08 11:49:15 -0800

    Housecleaning

--- UWB_Serial/UWB_Serial.py
+++ UWB_Serial/UWB_Serial.py
@@ -6,13 +6,14 @@
 ##
 ##
 
-# Library Imports
+###########      Library Imports       #############
 import serial
 
-# Debug Variable, set to true when you want to see all outputs
-Debug = True
+###########      Global Variables      #############
+Debug = True    # Set this to true for additional console outputs and data
 
 
+###########         Code Start         #############
 # Set up serial port and assign to 'ser'
 ser = serial.Serial()
 ser.baudrate = 9600
@@ -27,14 +28,9 @@ else:
     print(ser)   # verify serial port settings and activation   
 
 # DEBUG LINE
-# Read a line from 
-#while Debug:
-#    print(ser.readline())    # need to specify print
-
-# Read lines ad infinitum
-# Reads a line from the console and assigns byte data to currentLine
-# currentLine is parsed for the RX anchor ID and payload of 
-
+# Read lines from the serial port, print directly to console
+while Debug:
+    print(ser.readline())    # need to specify print
 
 
 
